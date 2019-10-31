99ca14d13c848a9b998e9e02d06ccde36dc29a80
w-udono <w-udono@starx.co.jp>
2019-10-31 10:33:05 +0900

    fix/event_machine_initializers

--- config/initializers/event_machine.rb
+++ config/initializers/event_machine.rb
@@ -1,3 +1,4 @@
+Thread.new { EventMachine.run } unless EventMachine.reactor_running? && EventMachine.reactor_thread.alive?
 unless EM.reactor_running? && EM.reactor_thread.alive?
   Thread.new { EM.run }
   while !EM.reactor_running?
